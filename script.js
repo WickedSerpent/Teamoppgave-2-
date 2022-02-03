889c34cd2b9f8c4ffd63ea770dd35dc77949038b
Atlefar <46597602+Atlefar@users.noreply.github.com>
2022-02-03 19:05:38 +0100

    Fungerende chatbot
    Ting den responderer på: 
    
    Hei og hopp
    Hvordan har du det?
    Hva heter du?
    Hva jobber du med??
    Hadet!

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-var userInput = document.getElementById("your-message");
-
-var input = document.getElementById("userText");
+const userInput = document.getElementById("your-message");
+const input = document.getElementById("userText");
 
 function submitText() {
   input.innerHTML = userInput.value;
   document.getElementById("your-message").value = "";
+}
+
+function chatbot() {
+  const bot = {
+    "Hei og hopp": "Hei på deg",
+    "Hvordan har du det?": "Jeg har det bare bra!",
+    "Hva heter du?": "Jeg heter Botleif",
+    "Hva jobber du med?": "Jeg jobber som hitech chatbot",
+    "Hadet!": "Hadet bra!",
+  };
+
+  const user = document.getElementById("your-message").value;
+
+  if (user in bot) {
+    document.getElementById("chatbotText").innerHTML = bot[user];
+  } else {
+    document.getElementById("chatbotText").innerHTML =
+      "Jeg forstår ikke hva du mener!";
+  }
 }
